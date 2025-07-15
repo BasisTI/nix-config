2217f5651515867b6850dcd1868a448e6f6fe7d9
Cédric Lamalle <cedric.lamalle@basis.com.br>
2025-07-15 14:24:34 -0300

    Remover jdk 8 do sdks.nix

--- sdks.nix
+++ sdks.nix
@@ -1,7 +1,8 @@
 {config, pkgs, ...}:
 {
   # JDK
-      "jdks/openjdk8".source = pkgs.jdk8;
+      # Erro: cycle detected in build of '/nix/store/pih5qinxqs3wbs2x8wjrnr39h8hp8yg2-openjdk-8u442-b06.drv' in the references of output 'jre' from output 'out
+      #"jdks/openjdk8".source = pkgs.jdk8;
       "jdks/openjdk11".source = pkgs.jdk11;
       "jdks/openjdk17".source = pkgs.jdk17;
       "jdks/openjdk21".source = pkgs.jdk21;
